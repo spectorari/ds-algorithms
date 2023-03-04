4aaf6c58b08ff398c9d6cbaf884c883fae6782bd
spectorari <spectorari@gmail.com>
2023-03-04 10:50:05 -0500

    commit #5
    - Fixed linting in IQDemo.java

--- SortingAlgorithms/src/queues/IQDemo.java
+++ SortingAlgorithms/src/queues/IQDemo.java
@@ -7,110 +7,102 @@ public class IQDemo {
 		FixedQueue q1 = new FixedQueue(10);
 		DynQueue q2 = new DynQueue(5);
 		CircularQueue q3 = new CircularQueue(10);
-		
+
 		ICharQ iQ;
-		
+
 		char ch;
 		int i;
-		
-		iQ=q1;
+
+		iQ = q1;
 		// Put some characters into fixed queue.
-		
+
 		try {
-		for(i=0; i < 10; i++)
-			iQ.put((char) ('A' + i));
-		}
-		catch (QueueFullException exc) {
+			for (i = 0; i < 10; i++)
+				iQ.put((char) ('A' + i));
+		} catch (QueueFullException exc) {
 			System.out.println(exc);
 		}
 		// SHow the queue.
 		System.out.print("Contents of fixed queue: ");
 		try {
-		for(i=0; i < 10; i++) {
-			ch = iQ.get();
-			System.out.print(ch);
-		}
-		}
-		catch (QueueEmptyException exc) {
+			for (i = 0; i < 10; i++) {
+				ch = iQ.get();
+				System.out.print(ch);
+			}
+		} catch (QueueEmptyException exc) {
 			System.out.println(exc);
 		}
 		System.out.println();
-		
+
 		iQ = q2;
 		// Put some characters into dynamic queue.
 		try {
-		for(i = 0; i < 10; i++)
-			iQ.put((char) ('Z' - i));
-		}
-		catch (QueueFullException exc) {
+			for (i = 0; i < 10; i++)
+				iQ.put((char) ('Z' - i));
+		} catch (QueueFullException exc) {
 			System.out.println(exc);
 		}
 		// Show the queue.
 		System.out.print("Contents of dynamic queue: ");
 		try {
-		for(i=0; i < 10; i++) {
-			ch = iQ.get();
-			System.out.print(ch);
-		}
-		}
-		catch (QueueEmptyException exc) {
+			for (i = 0; i < 10; i++) {
+				ch = iQ.get();
+				System.out.print(ch);
+			}
+		} catch (QueueEmptyException exc) {
 			System.out.println(exc);
 		}
 		System.out.println();
-		
+
 		iQ = q3;
 		// Put some characters into circular queue.
 		try {
-		for(i=0; i < 10; i++) 
-			iQ.put((char) ('A' + i));
-		}
-		catch (QueueFullException exc) {
+			for (i = 0; i < 10; i++)
+				iQ.put((char) ('A' + i));
+		} catch (QueueFullException exc) {
 			System.out.println(exc);
 		}
 		// Show the queue.
 		System.out.print("Contents of circular queue: ");
 		try {
-		for(i=0; i < 10; i++) {
-			ch = iQ.get();
-			System.out.print(ch);
-		}
-		}
-		catch (QueueEmptyException exc) {
+			for (i = 0; i < 10; i++) {
+				ch = iQ.get();
+				System.out.print(ch);
+			}
+		} catch (QueueEmptyException exc) {
 			System.out.println(exc);
 		}
 		System.out.println();
-		
+
 		// Put more characters into circular queue.
 		try {
-		for(i=10; i < 20; i++)
-			iQ.put((char) ('A' + i));
-		}
-		catch (QueueFullException exc) {
+			for (i = 10; i < 20; i++)
+				iQ.put((char) ('A' + i));
+		} catch (QueueFullException exc) {
 			System.out.println(exc);
 		}
 		// Show the queue.
 		System.out.print("Contents of circular queue: ");
 		try {
-		for(i=0; i < 10; i++) {
-			ch= iQ.get();
-			System.out.print(ch);
-		}
-		}
-		catch (QueueEmptyException exc) {
+			for (i = 0; i < 10; i++) {
+				ch = iQ.get();
+				System.out.print(ch);
+			}
+		} catch (QueueEmptyException exc) {
 			System.out.println(exc);
 		}
-		
+
 		System.out.println("\nStore and consume from circular queue.");
-		
+
 		// Store in and consume from circular queue.
 		try {
-		for(i=0; i < 20; i++) {
-			iQ.put((char) ('A' + i));
-			ch = iQ.get();
-			System.out.print(ch);		
-		}
-		}
-		catch (QueueFullException | QueueEmptyException exc) {
+			for (i = 0; i < 20; i++) {
+				iQ.put((char) ('A' + i));
+				ch = iQ.get();
+				System.out.print(ch);
+			}
+		} catch (QueueFullException | QueueEmptyException exc) {
 			System.out.println(exc);
 		}
-	}}
+	}
+}
